caa2db4e622f844ec920b63875db69af51b51463
robertpriedl <robert.priedl@hotmail.com>
2022-08-06 15:25:04 +0200

    init

---
+++ main.tf
@@ -0,0 +1,25 @@
+main.tf:
+
+provider "vsphere" {
+  user           = var.vsphere_user
+  password       = var.vsphere_password
+  vsphere_server = var.vsphere_server
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {}
+
+resource "vsphere_folder" "folder" {
+  path          = var.folder_name
+  type          = "vm"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+variable.tf:
+
+variable "vsphere_user" {}
+variable "vsphere_password" {}
+variable "vsphere_server" {}
+variable "folder_name" {}
